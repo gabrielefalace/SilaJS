a3920214aabbbf9e13ad2b6b85ac85f65f6744a7
unknown <gabrielefalace@gmail.com>
2015-03-02 09:25:35 +0100

    added JSDoc templates

--- sila.js
+++ sila.js
@@ -22,14 +22,28 @@
 	SOFTWARE.
 */
 
+/**
+ *
+ * @param element
+ * @returns {Predicate}
+ */
 function valueOf(element){
 	return new Predicate(element);
 }
 
+/**
+ *
+ * @param element
+ * @constructor
+ */
 function Predicate(element){
 	var self = this;
 	this.element = element;
 
+	/**
+	 *
+	 * @returns {boolean}
+	 */
 	self.isIn = function(){
 		for(var i=0; i<arguments.length; i++){
 			if(self.element === arguments[i]){
@@ -39,103 +53,197 @@ function Predicate(element){
 		return false;
 	};
 
+	/**
+	 *
+	 * @returns {boolean}
+	 */
 	self.isNotIn = function(){
 		return !self.isIn(arguments);
 	};
 
+	/**
+	 *
+	 * @param a
+	 * @param b
+	 * @returns {boolean}
+	 */
 	self.isBetween = function (a, b) {
 		return self.element > a && self.element < b;
 	};
 
+	/**
+	 *
+	 * @param a
+	 * @param b
+	 * @returns {boolean}
+	 */
 	self.isNotBetween = function (a, b) {
 		return !self.isBetween(a, b)
 	};
 }
 
 var matching = matching || {
-			
-	// equals for both String or Number
-	
-	equalsTo: function(param){ 
+
+		/**
+		 *
+		 * @param param
+		 * @returns {Function}
+		 */
+	equalsTo: function(param){
 		return function(element){return element === param;};
 	},
 
-	// for numbers
-	
-	odd: function(element){ 
-		return element % 2 === 1; 
+		/**
+		 *
+		 * @param element
+		 * @returns {boolean}
+		 */
+	odd: function(element){
+		return element % 2 === 1;
 	},
 
-	even: function(element){ 
-		return element % 2 === 0; 
+		/**
+		 *
+		 * @param element
+		 * @returns {boolean}
+		 */
+	even: function(element){
+		return element % 2 === 0;
 	},
-	
-	between: function(param1, param2){ 
+
+		/**
+		 *
+		 * @param param1
+		 * @param param2
+		 * @returns {Function}
+		 */
+	between: function(param1, param2){
 		return function(element){
 			return element >= param1 && element <= param2;
-		}; 
+		};
 	},
 
-	strictlyBetween: function(param1, param2){ 
+		/**
+		 *
+		 * @param param1
+		 * @param param2
+		 * @returns {Function}
+		 */
+	strictlyBetween: function(param1, param2){
 		return function(element){
 			return element > param1 && element < param2;
 		};
 	},
-	
+
+		/**
+		 *
+		 * @param param
+		 * @returns {Function}
+		 */
 	greaterThan: function(param){
-	    return function(element){ return element > param;};
+		return function(element){ return element > param;};
 	},
-	
+
+		/**
+		 *
+		 * @param param
+		 * @returns {Function}
+		 */
 	greaterEqualsTo: function(param){
 		return function(element){ return element >= param;};
 	},
-			
+
+		/**
+		 *
+		 * @param param
+		 * @returns {Function}
+		 */
 	smallerThan: function(param){
 		return function(element){return element < param;};
 	},
-	
+
+		/**
+		 *
+		 * @param param
+		 * @returns {Function}
+		 */
 	smallerEqualsTo: function(param){
 		return function(element){return element <= param;};
 	},
 
-	// for strings
-	
+		/**
+		 *
+		 * @param prefix
+		 * @returns {Function}
+		 */
 	startsWith: function(prefix){
 		return function(element){ return element.indexOf(prefix)===0; };
 	},
-	
+
+		/**
+		 *
+		 * @param substring
+		 * @returns {Function}
+		 */
 	contains: function(substring){
 		return function(element){ return element.indexOf(substring)>=0; };
 	},
-	
+
+		/**
+		 *
+		 * @param suffix
+		 * @returns {Function}
+		 */
 	endsWith: function(suffix){
 		return function(element){ return element.indexOf(suffix) + suffix.length === element.length; };
 	},
-	
-	// for objects
-	
+
+		/**
+		 *
+		 * @param fieldName
+		 * @param fieldValue
+		 * @returns {Function}
+		 */
 	fieldEquals: function(fieldName, fieldValue){
-		return function(element){ 
+		return function(element){
 			return element[fieldName] === fieldValue;
 		};
 	},
 
+		/**
+		 *
+		 * @param fieldName
+		 * @param prefix
+		 * @returns {Function}
+		 */
 	fieldStartsWith: function(fieldName, prefix){
 		return function(element) {
 			return matching.startsWith(prefix)(element[fieldName]);
 		};
 	},
 
+		/**
+		 *
+		 * @param fieldName
+		 * @param suffix
+		 * @returns {Function}
+		 */
 	fieldEndsWith: function(fieldName, suffix){
 		return function(element) {
 			return matching.endsWith(suffix)(element[fieldName]);
 		};
 	},
 
+		/**
+		 *
+		 * @param fieldName
+		 * @param substring
+		 * @returns {Function}
+		 */
 	fieldContains: function(fieldName, substring){
-		return function(element){						
+		return function(element){
 			return matching.contains(substring)(element[fieldName]);
 		};
 	}
 
-};
+}
